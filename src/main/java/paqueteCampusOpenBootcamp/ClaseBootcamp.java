a9513ddb62bb1fa2fc5a584a1c4233ebe7f2e6db
nicobritto <nicobritto21@gmail.com>
2022-08-29 17:02:08 -0300

    ejercicio

---
+++ src/main/java/paqueteCampusOpenBootcamp/ClaseBootcamp.java
@@ -0,0 +1,20 @@
+package paqueteCampusOpenBootcamp;
+
+public class ClaseBootcamp {
+
+    public static void main(String[] args) {
+
+        int numerito = 0;
+        long numerito1 = 1;
+        double numerito2 = 1.2;
+        boolean verdad = true;
+        String palabra = "Nico";
+        
+        System.out.println(numerito);
+        System.out.println(numerito1);
+        System.out.println(numerito2);
+        System.out.println(verdad);
+        System.out.println(palabra);
+
+    }
+}
